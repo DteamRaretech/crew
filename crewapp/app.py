9dd39f16276693b5c4b68d7c8341a4f249d8a042
junya-f <fujiokajunya@icloud.com>
2023-07-25 21:23:57 +0900

    バックエンドスタート

--- crewapp/app.py
+++ crewapp/app.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from flask import Flask
+app = Flask(name)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+#サインアップ処理
+
+#ログイン・ログアウト処理
+
+#googleAPI連携
+
+if name == 'main':
+    app.run(debug=True)
