a41d11db42e90c8e353abeda18d37280e9c9bf2a
Sen <ffix.3000.c@gmail.com>
2023-07-25 22:38:51 +0900

    20230725 奮闘記録

--- crewapp/app.py
+++ crewapp/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
-app = Flask(name)
+from flask import Flask, request, redirect, render_template, session, flash, abort
+app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+# サインアップページの表示
+@app.route('/signup')
+def signup():
+    return render_template('registation/signup.html')
 
 #サインアップ処理
 
@@ -11,5 +18,5 @@ def hello_world():
 
 #googleAPI連携
 
-if name == 'main':
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
--- crewapp/models.py
+++ crewapp/models.py
@@ -1,1 +1,10 @@
-"DBconnect.関数をまとめる"
+# データベースを操作する関数をmodels.pyにまとめる
+# from モジュール import クラス で、クラスメソッドを呼び出せる状態にする
+
+import pymysql.cursors # pymysqlライブラリを取り込む
+from util.DB import DB # util.DB から DBクラスを取り込む
+
+# pymysqlと接続後、各sqlを実行するクラスを宣言
+class dbConnect:
+
+        # 各sqlを実行する関数を作成…クラス内で作成した関数をmethodと呼ぶ
