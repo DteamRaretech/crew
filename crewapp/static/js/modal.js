3d1967959212c43509c9fcef2f1d4a031cdc3a75
daibu-28 <sakuranbo2028@yahoo.co.jp>
2023-07-27 08:58:49 +0900

    20230727 shiba

--- crewapp/static/js/modal.js
+++ crewapp/static/js/modal.js
@@ -1,34 +1,25 @@
-function validateForm() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    if (username === "" || password === "") {
-      displayErrorModal("Please enter both username and password");
-      return false; // フォームの送信を中止
-    }
-  
-    // ログインの処理を行う場合は、ここに追加のコードを記述
-  
-    return true; // フォームを送信
+// モーダル表示をトリガーとするボタンの要素を取得
+const modalButton = document.getElementById("modalButton");
+
+// モーダル要素を取得
+const modal = document.getElementById("myModal");
+
+// モーダルを閉じる要素を取得
+const closeSpan = document.getElementsByClassName("close")[0];
+
+// モーダル表示をトリガーとするボタンにクリックイベントを追加
+modalButton.addEventListener("click", () => {
+  modal.style.display = "block";
+});
+
+// モーダルを閉じる要素にクリックイベントを追加
+closeSpan.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// モーダル外の領域をクリックしてもモーダルを閉じる
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-  
-  function displayErrorModal(errorMessage) {
-    var modal = document.getElementById("modal");
-    var modalContent = document.querySelector(".modal-content");
-    var errorMessageElement = document.getElementById("errorMessage");
-  
-    errorMessageElement.textContent = errorMessage;
-    modal.style.display = "block";
-  
-    var closeBtn = document.getElementsByClassName("close")[0];
-    closeBtn.onclick = function() {
-      modal.style.display = "none";
-    };
-  
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    };
-  }
-  
+});
