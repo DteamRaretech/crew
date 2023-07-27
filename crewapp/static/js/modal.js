e1e43a2536c365c5328d3d197d644ebca58536ef
daibu-28 <sakuranbo2028@yahoo.co.jp>
2023-07-27 11:01:02 +0900

    modal作成

--- crewapp/static/js/modal.js
+++ crewapp/static/js/modal.js
@@ -1,25 +1,23 @@
-// モーダル表示をトリガーとするボタンの要素を取得
-const modalButton = document.getElementById("modalButton");
+'use strict';
 
-// モーダル要素を取得
-const modal = document.getElementById("myModal");
+{
+  const open = document.getElementById('modal-open');
+  const container = document.getElementById('modal-container');
+  const modalBG = document.getElementById('modal-bg');
+  const close = document.getElementById('modal-close');
 
-// モーダルを閉じる要素を取得
-const closeSpan = document.getElementsByClassName("close")[0];
+  open.addEventListener('click', () => {
+      container.classList.add('active');
+      modalBG.classList.add('active');
+  });
 
-// モーダル表示をトリガーとするボタンにクリックイベントを追加
-modalButton.addEventListener("click", () => {
-  modal.style.display = "block";
-});
-
-// モーダルを閉じる要素にクリックイベントを追加
-closeSpan.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-// モーダル外の領域をクリックしてもモーダルを閉じる
-window.addEventListener("click", (event) => {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-});
+  close.addEventListener('click', () => {
+      container.classList.remove('active');
+      modalBG.classList.remove('active');
+  });
+  
+  modalBG.addEventListener('click', () => {
+      container.classList.remove('active');
+      modalBG.classList.remove('active');
+  });
+}
